f2fbccd09127055bc5253d3cd84619a7c1b9dc32
ArthurV <valiente.arthur@gmail.com>
2020-01-03 20:17:06 +0100

    Add option to sample in order to choose the time function

--- python/toolbox/sample.py
+++ python/toolbox/sample.py
@@ -29,8 +29,10 @@ class sample:
     ---------
     __function: function
         The function wrapped by the decorator
+    __get_time: function
+        The function use to get the current time (default to time.time)
     __last_call: float
-        The last time we called it (time.perf_counter())
+        The last time we called __get_time()
     __variance_tmp: float
         The temporary variable used to compute the variance
     _updated: threading.Condition
@@ -48,11 +50,10 @@ class sample:
         The variance period computed recursivelly in sec
     _stddev_period: float
         The stddev period computed recursivelly in sec (_variance_period)^(1/2)
-
     """
-    def __init__(self, function):
-        functools.update_wrapper(self, function)
-        self.__function = function
+    def __init__(self, _function = None, *, time_function = time.time):
+        self.__function = _function
+        self.__get_time = time_function
         self._updated = threading.Condition()
         self._reset_sampling()
 
@@ -82,41 +83,60 @@ class sample:
             self._mean_period = None
 
     def __call__(self, *args, **kwargs):
-        with self._updated:
-            now = time.perf_counter()
+        if not self.__function:
+            # function is None, it means that sample has been initialize with
+            # an argument therefore the first __call__ contains the function to
+            # wrap around
+            # i.e.:
+            # @sample(time_function = time.time)
+            # def toto():
+            #   pass
+            #
+            # is equivalent to:
+            # toto = sample(time_measurement_function = time.time)(toto)
 
-            if self.__last_call is not None:
+            functools.update_wrapper(self, args[0])
+            self.__function = args[0]
 
-                # Measurement Xn
-                self._last_period = now - self.__last_call
+            return self
 
-                if self._n_measurement == 0:  # Initialize
-                    # Initialization with the first measurement
-                    self._mean_period = self._last_period
-                    self.__variance_tmp = 0
+        else:
+            # Here normal function call
+            with self._updated:
+                now = self.__get_time()
 
-                else:       # Iterative computation
-                    # Compute Mean ( M_n+1 = M_n + (X_n+1 - M_n)/(n+1))
-                    # NOTE: (_n_call) is equals to (_n_measurement + 1)
-                    new_mean = \
-                        self._mean_period + \
-                        ((self._last_period - self._mean_period) / \
-                         self._n_call)
+                if self.__last_call is not None:
 
-                    # Compute the tmp variance V* (false variance)
-                    # V*_n+1 = V*_n + (X_n+1 - M_n)x(X_n+1 - M_n+1)
-                    # True variance V: V_n+1 = V*/(n)
-                    self.__variance_tmp += \
-                        ((self._last_period - self._mean_period) *\
-                         (self._last_period - new_mean))
+                    # Measurement Xn
+                    self._last_period = now - self.__last_call
 
-                    self._mean_period = new_mean
+                    if self._n_measurement == 0:  # Initialize
+                        # Initialization with the first measurement
+                        self._mean_period = self._last_period
+                        self.__variance_tmp = 0
 
-                # Notify new measurement
-                self._updated.notify_all()
-                self._n_measurement += 1
+                    else:       # Iterative computation
+                        # Compute Mean ( M_n+1 = M_n + (X_n+1 - M_n)/(n+1))
+                        # NOTE: (_n_call) is equals to (_n_measurement + 1)
+                        new_mean = \
+                            self._mean_period + \
+                            ((self._last_period - self._mean_period) / \
+                             self._n_call)
 
-            self.__last_call = now
-            self._n_call += 1
+                        # Compute the tmp variance V* (false variance)
+                        # V*_n+1 = V*_n + (X_n+1 - M_n)x(X_n+1 - M_n+1)
+                        # True variance V: V_n+1 = V*/(n)
+                        self.__variance_tmp += \
+                            ((self._last_period - self._mean_period) *\
+                             (self._last_period - new_mean))
 
-        return self.__function(*args, **kwargs)
+                        self._mean_period = new_mean
+
+                    # Notify new measurement
+                    self._updated.notify_all()
+                    self._n_measurement += 1
+
+                self.__last_call = now
+                self._n_call += 1
+
+            return self.__function(*args, **kwargs)
