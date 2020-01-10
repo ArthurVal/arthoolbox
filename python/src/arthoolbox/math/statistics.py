771d1f6356d693f780e0aa62e6d704772a095793
ArthurV <valiente.arthur@gmail.com>
2020-01-10 10:31:44 +0100

    Add update_variance function to statistics + tests

--- python/src/arthoolbox/math/statistics.py
+++ python/src/arthoolbox/math/statistics.py
@@ -2,30 +2,71 @@
 """Module containing specifics statistics function like recursive computation...
 
 Function list:
-- update_mean() | Compute an updated mean with the old mean and new measurement
-- update_variance() | Compute a new variance TODO
+- update_mean()     | Compute an updated mean
+- update_variance() | Compute a new variance
 """
 
 def update_mean(new_data, old_mean, num_data):
     """Compute a new mean recursively using the old mean and new measurement
 
-    From the arithmetic mean computed using the n measurements (M_n), we compute
-    the new mean (M_n+1) adding a new measurement (X_n+1) with the formula:
-    M_n+1 = M_n+1 = M_n + (X_n+1 - M_n)/(n+1)
+    From the arithmetic mean computed using the n-1 measurements (M_n-1), we
+    compute the new mean (M_n) adding a new measurement (X_n) with the formula:
+    M_n = M_n-1 + (X_n - M_n-1)/n
 
+    See: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
 
     Parameters
     ----------
     new_data: int or decimal
-        The new measurement Xn+1
+        The new measurement Xn
     old_mean: int or decimal
-        The mean Mn computed previously
+        The mean Mn-1 computed previously
     num_data:
         The number of data used to compute Mn
 
     Returns
     -------
     float
-        The new mean Mn+1 updated with Xn+1
+        The new mean Mn updated with Xn
     """
-    return (old_mean + (new_data - old_mean) / (num_data + 1))
+    return (old_mean + (new_data - old_mean) / num_data)
+
+def update_variance(new_data, old_variance, new_mean, old_mean, num_data):
+    """Compute a new variance recursively using the old and new mean
+
+    From the previously computed variance V_n-1, the new and old mean M_n and
+    M_n-1 and the new measurement X_n, we compute an update value of the new
+    variance using the formula:
+    V_n = V_n-1 + (((X_n - M_n)*(X_n - M_n-1) - V_n-1) / n)
+
+    See: https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+    Notes
+    -----
+    This formula may be unstable due to the floating point calculation issues.  
+
+    Parameters
+    ----------
+    new_data: int or decimal
+        The new measurement X_n
+    old_variance: int or decimal
+        The variance V_n-1 computed previously
+    new_mean: int or decimal
+        The mean M_n computed on the current step n
+    old_mean: int or decimal
+        The mean M_n-1 computed previously
+    num_data:
+        The number of total data n
+
+    Returns
+    -------
+    float
+        The new variance M_n updated with X_n, V_n-1, M_n and M_n-1
+    """
+    return (
+        old_variance +
+        (
+            ((new_data - old_mean) * (new_data - new_mean) - old_variance)
+            / num_data
+        )
+    )
--- python/tests/arthoolbox/math/test_statistics.py
+++ python/tests/arthoolbox/math/test_statistics.py
@@ -12,8 +12,7 @@ def test_update_mean(data):
 
     log.debug("Use the update_mean function on {} data".format(len(data)))
     computed_mean = 0
-    for i, single_data in enumerate(data):
-
+    for i, single_data in enumerate(data, start = 1):
 
         computed_mean = arthstats.update_mean(
             new_data = single_data,
@@ -23,9 +22,49 @@ def test_update_mean(data):
 
         log.debug("{} -> X:{} | Mean: {}".format(i, single_data, computed_mean))
 
-        assert math.isclose(st.mean(data[:(i+1)]), computed_mean, rel_tol = 0.005), \
-            ("The recursive mean computed on step n = {} don't match the"
+        assert math.isclose(st.mean(data[:(i)]), computed_mean, rel_tol = 0.005), \
+            ("The recursive mean computed on step n = {} doesn't match the"
              " arithmetic mean computed with statistics.mean").format(i)
 
+
+    log.info("{0:*^50}".format(" DONE "))
+
+
+def test_update_variance(data):
+    log = module_log.getChild('update_variance')
+    log.info("{0:*^50}".format(" Testing update_variance function "))
+
+    log.debug("Use the update_variance function on {} data".format(len(data)))
+    i_mean, i_1_mean, computed_variance = 0.0, data[0], 0.0
+    for i, single_data in enumerate(data[1:], start = 2):
+
+        i_mean = arthstats.update_mean(
+            new_data = single_data,
+            old_mean = i_1_mean,
+            num_data = i
+        )
+
+        computed_variance, i_1_mean = (
+            arthstats.update_variance(
+                new_data = single_data,
+                old_variance = computed_variance,
+                new_mean = i_mean,
+                old_mean = i_1_mean,
+                num_data = i
+            ),
+            i_mean
+        )
+
+        log.debug(
+            ("{} -> X:{} | Mean: {} | Variance: {}")
+            .format(i, single_data, i_mean, computed_variance)
+        )
+
+        assert math.isclose(
+            st.pvariance(data[:(i)], mu = i_mean),
+            computed_variance,
+            rel_tol = 0.005), \
+            ("The recursive variance computed on step n = {} doesn't match the"
+             " arithmetic variance computed with statistics.variance").format(i)
 
     log.info("{0:*^50}".format(" DONE "))
