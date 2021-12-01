30ad42b4798f46f259b0ab00b742f6fa3edeb589
Arthur Valiente <arthur.valiente@esrf.fr>
2021-12-01 14:59:14 +0100

    Add benchmarks for Mean and Variance

---
+++ cxx/benchmarks/bench_statistics.cpp
@@ -0,0 +1,74 @@
+#include <benchmark/benchmark.h>
+
+#include "arthoolbox/math/statistics.hpp"
+
+#include <iterator>
+#include <random>
+#include <vector>
+
+namespace arthoolbox {
+namespace stats {
+
+std::vector<double> generateRandomNormalDistribution(double mean, double stddev,
+                                                     std::size_t n) noexcept {
+  std::random_device seed_generator;
+  std::mt19937 random_generator(seed_generator());
+  std::normal_distribution<double> distribution(mean, stddev);
+
+  std::vector<double> out;
+  out.reserve(n);
+
+  std::generate_n(std::back_inserter(out), n,
+                  [&distribution, &random_generator]() {
+                    return distribution(random_generator);
+                  });
+
+  return out;
+}
+
+const std::vector<double> &getRandomData() {
+  static auto out = generateRandomNormalDistribution(42, 5e-3, 500);
+  return out;
+}
+
+static void BM_ComputeRecurringMean(benchmark::State &state) {
+  const auto& data = getRandomData();
+  for (auto _ : state) {
+    double mean = 0.0;
+    for(auto i = 0; i < data.size(); i++) {
+      const auto &sample = data[i];
+      const auto n = i + 1;
+
+      mean = update_recurring_mean(sample, mean, i + 1);
+    }
+    benchmark::DoNotOptimize(mean);
+  }
+}
+BENCHMARK(BM_ComputeRecurringMean);
+
+static void BM_ComputeRecurringMeanVariance(benchmark::State &state) {
+  const auto& data = getRandomData();
+  for (auto _ : state) {
+    double mean = 0.0;
+    double variance = 0.0;
+
+    for(auto i = 0; i < data.size(); i++) {
+      const auto &sample = data[i];
+      const auto n = i + 1;
+      const auto old_mean = mean;
+
+      mean = update_recurring_mean(sample, mean, n);
+      variance = update_recurring_variance(sample, variance, mean, old_mean, n);
+    }
+
+    benchmark::DoNotOptimize(mean);
+    benchmark::DoNotOptimize(variance);
+  }
+}
+BENCHMARK(BM_ComputeRecurringMeanVariance);
+
+
+} // namespace stats
+} // namespace arthoolbox
+
+BENCHMARK_MAIN();
